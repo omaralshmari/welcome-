bc0bff45c4cb61484bfa2e9a849835f15d215518
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-31 17:57:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,6 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'zg') {
-        message.reply('pong');
-      }
-});
 
   const lol =
 [
